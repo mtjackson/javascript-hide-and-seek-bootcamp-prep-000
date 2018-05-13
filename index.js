0765e25a04bb13b26fd4a59d90baf6a5110d2962
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-13 15:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function increaseRankBy(n) {
   
   for (let i = 0; i < lis.length; i++) {
     let x = parseInt(lis[i].innerHTML, 10);
+    parseInt(n, 10);
     lis[i].innerHTML = (x + n).toString()
   }
 }
