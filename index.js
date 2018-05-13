5eefe6d1c2310de0a89361c811e02e92afd0684b
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-13 14:50:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   // pulls a .target out of #nested 
 }
+
+function increaseRankBy(n) {
+  // incresases the ranks of all the .ranked-lists by n 
+  // may need to us parseInt()
+}
+
+function deepestChild() {
+  // pulls out the most deeply nestd child from div#grand-node
+}
